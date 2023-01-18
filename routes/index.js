53dfb08efc5f24d619b4998188e49ca96a9072cf
Muhammad Fikri <fikrimuhammad2016@gmail.com>
2023-01-18 23:06:16 +0700

    update connect to mongodb

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import Url from '../models/Url.js';
+const router = express.Router();
+
+router.get('/:urlId', async (req, res) => {
+  try {
+    const url = await Url.findOne({ urlId: req.params.urlId });
+    if (url) {
+      await Url.updateOne(
+        {
+          urlId: req.params.urlId,
+        },
+        { $inc: { clicks: 1 } }
+      );
+      setTimeout(() => {
+        return res.redirect(url.origUrl);
+      }, 3000);
+    } else res.status(404).json('Not found');
+  } catch (err) {
+    console.log(err);
+    res.status(500).json('Server Error');
+  }
+});
+
+export default router;
