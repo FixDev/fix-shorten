bf7448a7e9a2d625e193dbc056e0c03fb1550bdb
Muhammad Fikri <fikrimuhammad2016@gmail.com>
2023-01-20 21:14:41 +0700

    add default routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
-import UrlContoller from '../controller/UrlController.js'
+import UrlContoller from '../controller/UrlController.js';
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  res.send('Helloo ini api');
+});
 router.get('/:urlId', UrlContoller.index);
 router.post('/api/short', UrlContoller.short);
 
